747e59801ebbf9cb011cfccd28f9ce4446b9c37c
Piotr Mościcki <m.piotr@moscicki.pl>
2021-10-24 14:27:04 +0000

    Add build configuration

---
+++ src/assets/assets.cpp
@@ -0,0 +1,8 @@
+#include "assets.hpp"
+
+#include <iostream>
+
+//*************************************************************************************************
+void assets::print_test(const std::string& content) {
+    std::cout << "asset_manager test: " << content << std::endl;
+}
---
+++ src/assets/assets.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+
+namespace assets {
+
+    void print_test(const std::string& content);
+
+} // namespace assets
