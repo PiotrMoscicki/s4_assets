defaac9279e2daaab592c389cb80be21e4b5a06b
Piotr Mościcki <m.piotr@moscicki.pl>
2021-10-24 14:39:27 +0000

    Add uuid library

--- test/src/asset.cpp
+++ test/src/asset.cpp
@@ -1,6 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
 
+#include <uuid.h>
+
 //*************************************************************************************************
 TEST_CASE( "Math test", "[Asset]" ) {
     REQUIRE( 1 + 2 == 3 );
+}
+
+//*************************************************************************************************
+TEST_CASE( "Uuid library test", "[Asset]" ) {
+    uuids::uuid empty;
+    REQUIRE( empty.is_nil() == true );
 }
